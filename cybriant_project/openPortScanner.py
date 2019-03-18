06641c148d17f6e95e93e40ed23e9bf86b8eac4f
caticide <42981437+caticide@users.noreply.github.com>
2019-03-18 15:24:47 -0400

    Update openPortScanner.py

--- cybriant_project/openPortScanner.py
+++ cybriant_project/openPortScanner.py
@@ -4,8 +4,7 @@ import subprocess
 import sys
 from datetime import datetime
 
-# Clear the screen
-subprocess.call('clear', shell=True)
+
 
 # Ask for input
 remoteServer    = input("Enter a remote host to scan: ")
