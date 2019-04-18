7b0f93080fedd9d3151172687bfe5d175dc5cdee
Parth Patel <parthpatel2415@gmail.com>
2019-04-17 23:21:16 -0400

    amazons3

---
+++ amazon_s3.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
+
+s3 = boto3.client('s3')
+
+response = s3.list_buckets()
+
+for bucket in response['Buckets']:
+  try:
+    enc = s3.get_bucket_encryption(Bucket=bucket['Name'])
+    rules = enc['ServerSideEncryptionConfiguration']['Rules']
+    print('Bucket: %s, Encryption: %s' % (bucket['Name'], rules))
+  except ClientError as e:
+    if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+      print('Bucket: %s, no server-side encryption' % (bucket['Name']))
+    else:
+      print("Bucket: %s, unexpected error: %s" % (bucket['Name'], e))
