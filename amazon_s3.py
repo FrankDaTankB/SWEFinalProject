006c35f1d46775b5d35522506c7fbd171720fcba
Parth Patel <parthpatel2415@gmail.com>
2019-04-21 11:03:30 -0400

    Update amazon_s3.py

--- amazon_s3.py
+++ amazon_s3.py
@@ -4,17 +4,18 @@ import os
 
 os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
 
-s3 = boto3.client('s3')
-
-response = s3.list_buckets()
-
-for bucket in response['Buckets']:
-  try:
-    enc = s3.get_bucket_encryption(Bucket=bucket['Name'])
-    rules = enc['ServerSideEncryptionConfiguration']['Rules']
-    print('Bucket: %s, Encryption: %s' % (bucket['Name'], rules))
-  except ClientError as e:
-    if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
-      print('Bucket: %s, no server-side encryption' % (bucket['Name']))
-    else:
-      print("Bucket: %s, unexpected error: %s" % (bucket['Name'], e))
+def checkBucketEncryption(aws_access_key_id, aws_secret_access_key):
+    s3 = boto3.client('s3',
+                      aws_access_key_id=accessKey,
+                      aws_secret_access_key=secretKey,
+                      )
+    response = s3.list_buckets()
+    for bucket in response['Buckets']:
+        try:
+            enc = s3.get_bucket_encryption(Bucket=bucket['Name'])
+            print('Bucket: %s, Encryption: %s' % (bucket['Name'], rules))
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ServerSideEncryptionConfigurationNotFoundError':
+                print('Bucket: %s, no server-side encryption' % (bucket['Name']))
+            else:
+                print("Bucket: %s, unexpected error: %s" % (bucket['Name'], e))
