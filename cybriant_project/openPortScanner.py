e7e4e8124d2d12fb43c4604c25c9db0ade588f01
caticide <42981437+caticide@users.noreply.github.com>
2019-04-24 16:33:26 -0400

    demo version of the open port modeual
    adsasd

--- cybriant_project/openPortScanner.py
+++ cybriant_project/openPortScanner.py
@@ -3,12 +3,14 @@ import socket
 import subprocess
 import sys
 from datetime import datetime
-
+import threading
+import os
 
 
 # Ask for input
 remoteServer    = input("Enter a remote host to scan: ")
 remoteServerIP  = socket.gethostbyname(remoteServer)
+
 
 # Print a nice banner with information on which host we are about to scan
 print ("-" * 60)
@@ -16,20 +18,44 @@ print ("Please wait, scanning remote host", remoteServerIP)
 print ("-" * 60)
 
 # Check what time the scan started
-t1 = datetime.now()
+time1 = datetime.now()
+
+
 
 # Using the range function to specify ports (here it will scans all ports between 1 and 1024)
 
 # We also put in some error handling for catching errors
-
+print ("checking Normal Usage ports")
 try:
-    for port in range(1,1025):
+    for port in range(75,90):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(0.5)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
             print ("Port {}: 	 Open".format(port))
         sock.close()
+except KeyboardInterrupt:
+    print ("You pressed Ctrl+C")
+    sys.exit()
+
+except socket.gaierror:
+    print ('Hostname could not be resolved. Exiting')
+    sys.exit()
+
+except socket.error:
+    print ("Couldn't connect to server")
+    sys.exit()
+
+try:
+    for port in range(440,450):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print ("Port {}: 	 Open".format(port))
+        sock.close()
+
+
 
 except KeyboardInterrupt:
     print ("You pressed Ctrl+C")
@@ -43,11 +69,12 @@ except socket.error:
     print ("Couldn't connect to server")
     sys.exit()
 
+
 # Checking the time again
-t2 = datetime.now()
+time2 = datetime.now()
 
 # Calculates the difference of time, to see how long it took to run the script
-total =  t2 - t1
+total =  time2 - time1
 
 # Printing the information to screen
-print ('Scanning Completed in: ', total)
+print ('Quick Scan Completed in: ', total)
